fb2e62cb6fecb56d79d1aebc19f2d690531a7728
Fateme Zamanian <fatemehzamanian.1999@gmail.com>
2021-09-18 14:24:42 +0430

    fix collision

--- Super Snake.py
+++ Super Snake.py
@@ -1,3 +1,4 @@
+import time
 import random
 import arcade
 from arcade import check_for_collision
@@ -6,47 +7,47 @@ class Apple(arcade.Sprite):
     def __init__(self):
         arcade.Sprite.__init__(self)
         self.r=10
-        self.x=random.randint(40,560)
-        self.y=random.randint(40,560)
+        self.center_x=random.randint(40,560)
+        self.center_y=random.randint(40,560)
         self.color=arcade.color.RED
         self.width=20
         self.height=20
     def show(self):
-        arcade.draw_circle_filled(self.x, self.y, self.r, self.color)
+        arcade.draw_circle_filled(self.center_x, self.center_y, self.r, self.color)
         
 
 class Pear(arcade.Sprite):
     def __init__(self):
         arcade.Sprite.__init__(self)
         self.r=10
-        self.x=random.randint(40,560)
-        self.y=random.randint(40,560)
+        self.center_x=random.randint(40,560)
+        self.center_y=random.randint(40,560)
         self.color=arcade.color.YELLOW
         self.height=20
         self.width=20
     def show(self):
-        arcade.draw_circle_filled(self.x, self.y, self.r, self.color)
+        arcade.draw_circle_filled(self.center_x, self.center_y, self.r, self.color)
 
 class Bomp(arcade.Sprite):
     def __init__(self):
         arcade.Sprite.__init__(self)
         self.r=10
-        self.x=random.randint(40,560)
-        self.y=random.randint(40,560)
+        self.center_x=random.randint(40,560)
+        self.center_y=random.randint(40,560)
         self.color=arcade.color.WHITE
         self.height=20
         self.width=20
     def show(self):
-        arcade.draw_circle_filled(self.x, self.y, self.r, self.color)
+        arcade.draw_circle_filled(self.center_x, self.center_y, self.r, self.color)
 
 
 
 class Snake(arcade.Sprite):
     def __init__(self):
         arcade.Sprite.__init__(self)
-        self.x=300
-        self.y=300
-        self.color2=arcade.color.GREEN
+        self.center_x=300
+        self.center_y=300
+        self.color2=arcade.color.BLUE
         self.color3=arcade.color.YELLOW
         self.color1=arcade.color.RED
         self.x_change=0
@@ -54,35 +55,34 @@ class Snake(arcade.Sprite):
         self.width=16
         self.height=16
         self.score=0
-        self.r=8
+        self.r=10
         self.speed=1
         self.body=[]
     
     def show(self):
-        arcade.draw_circle_filled(self.x,self.y,self.r,self.color1)
         i=0
         for b in self.body:
             i+=1
             if i%2==0:
-                arcade.draw_circle_filled(b['x'],b['y'],self.r,self.color1)
-            else:
                 arcade.draw_circle_filled(b['x'],b['y'],self.r,self.color2)
-        
+            else:
+                arcade.draw_circle_filled(b['x'],b['y'],self.r,self.color3)
+        arcade.draw_circle_filled(self.center_x,self.center_y,self.r,self.color1)
 
-    def on_update(self, delta_time: float = 1/60):
-        # self.center_x += self.speed * self.change_x
-        # self.center_y += self.speed * self.change_y
-        self.body.append({'x':self.x,'y':self.y})
+    def on_update(self, delta_time: float = 1/40):
+        self.body.append({'x':self.center_x,'y':self.center_y})
         if len(self.body)>self.score:
             self.body.remove(self.body[0])
-        if self.x_change==-1:
-            self.x-=self.speed
-        elif self.x_change==1:
-            self.x+=self.speed
-        elif self.y_change==-1:
-            self.y-=self.speed
-        elif self.y_change==1:
-            self.y+=self.speed
+        # if self.x_change==-1:
+        #     self.center_x-=self.speed
+        # elif self.x_change==1:
+        #     self.center_x+=self.speed
+        # elif self.y_change==-1:
+        #     self.center_y-=self.speed
+        # elif self.y_change==1:
+        #     self.center_y+=self.speed
+        self.center_x += self.speed * self.x_change
+        self.center_y += self.speed * self.y_change
     
     
 
@@ -97,11 +97,16 @@ class Game(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        if self.lose():
+            arcade.set_background_color(arcade.color.RED)
+            arcade.draw_text('Game Over', 220, 300, arcade.color.WHITE, 20, 20)
+            time.sleep(5)
+            return
         self.snake.show()
         self.apple.show()
         self.pear.show()
         self.bomb.show()
-        arcade.draw_text(text=f'Score: {self.snake.score}',start_x=0 ,start_y=250, width=600, font_size=20, align="center", color=arcade.color.WHITE)
+        arcade.draw_text(text=f'Score: {self.snake.score}',start_x=0,start_y=560,width=600, font_size=20, align="center", color=arcade.color.WHITE)
 
 
     def on_key_release(self, key, modifiers):
@@ -128,25 +133,20 @@ class Game(arcade.Window):
         self.snake.on_update(delta_time)
         
         if check_for_collision(self.snake,self.apple):
-            # self.apple=Apple()
+            self.apple=Apple()
             self.snake.score+=1
 
         if check_for_collision(self.snake,self.pear):
-            # self.pear=Pear()
+            self.pear=Pear()
             self.snake.score+=2
 
         if check_for_collision(self.snake,self.bomb):
-            # self.bomb=Bomp()
+            self.bomb=Bomp()
             self.snake.score -=1
             
-        if self.lose():
-            arcade.set_background_color(arcade.color.BLACK)
-            arcade.draw_text('Game Over', 300, 300, arcade.color.WHITE, 20, 20)
         
-
-
     def lose(self):
-        if self.snake.x>=590 or self.snake.x<=10 or self.snake.y>=590 or self.snake.y<=10 or self.snake.score<0:
+        if self.snake.center_x>=590 or self.snake.center_x<=10 or self.snake.center_y>=590 or self.snake.center_y<=10 or self.snake.score<0:
             return True
         else:
             return False
